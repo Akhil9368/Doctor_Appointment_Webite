d730a235978f90858e82b87cf688776f839cb869
Akhil <akhi.soni@oracle.com>
2024-01-22 23:15:22 +0530

    Basic Code

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+'use strict';
+// /* The above statement tells the browser to use the Strict mode application for Js file or any specific function, just to make your code safe.
+// Let's understand it:
+// It is introduced in ECMAScript 5 and allows us to place it in JavaScript file or in a function to make them follow some "strict" rules during execution.
+// It helps us to prevents some unnecessary action to happen and helps us to throws more exceptions in detail so that we could have more clear and safe script codes.*/
+
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+// PreLoader
+const preloader = document.querySelector("[data-preloader]");
+window.addEventListener("load", function(){
+    preloader.classList.add("loaded");
+    document.body.classList.add("loaded");
+});
+
+
+let navbar = document.querySelector("[data-navbar]");
+let navTogglers = document.querySelectorAll("data-nav-toggler");
+let overlay = document.querySelector("[data-overlay]");
+const open_btn = document.getElementsByClassName(".nav-open-btn");
+
+// console.log(open_btn)
+function Open_close(){
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+
+
+
+/**
+ * HEADER & BACK TOP BTN
+ * 
+ * active header & back top btn when window scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+
+const activeElementOnScroll = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+}
+
+window.addEventListener("scroll", activeElementOnScroll);
